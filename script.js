e413f869e243b6e1b1f114258cd892f4cba2e544
Star <73551567+Star69995@users.noreply.github.com>
2024-10-29 15:17:54 +0200

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const projects = [
+    { name: "קריפטוגרמה", url: "cryptogram/index.html" },
+    { name: "שעון עצר", url: "timer/index.html" },
+];
+
+const container = document.getElementById('projects-container');
+
+projects.forEach(project => {
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'project';
+    projectDiv.innerHTML = `<a href="${project.url}">${project.name}</a>`;
+    container.appendChild(projectDiv);
+});
