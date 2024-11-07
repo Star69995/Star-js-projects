8714ffe338b86ad69b2951b62043028ec17759c0
Star <73551567+Star69995@users.noreply.github.com>
2024-11-07 15:24:34 +0200

    .

--- cryptogram/script.js
+++ cryptogram/script.js
@@ -674,12 +674,3 @@ function showKeyboard() {
     button.textContent = "הסתר מקלדת";
 }
 
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
