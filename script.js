b4a164637e74abba4bd95435de59b539fa997854
Star <73551567+Star69995@users.noreply.github.com>
2025-02-23 12:04:03 +0200

    crossword

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const projects = [
     { name: "האתר של סטאר", url: "https://star69995.github.io/star-site/" },
-    { name: "קריפטוגרמה", url: "cryptogram/" },
-    { name: "שעון עצר", url: "timer/" },
-    { name: "מנהל משימות", url: "task_manager/"},
-    { name: "משחק מתמטי", url: "mathGame/"},
-    {name: "מזג אוויר", url: "weather/"},
-    {name: "בונה דפים", url: "pageBuilder/"},
-    {name: "מחשבון קרשים", url: "Mai's calc/"},
+    { name: "תשחץ", url: "https://star69995.github.io/star-crossword/" },
+    { name: "קריפטוגרמה", url: "https://star69995.github.io/star-site/js-projects/cryptogram/" },
+    { name: "שעון עצר", url: "https://star69995.github.io/star-site/js-projects/timer/" },
+    { name: "מנהל משימות", url: "https://star69995.github.io/star-site/js-projects/task_manager/"},
+    { name: "משחק מתמטי", url: "https://star69995.github.io/star-site/js-projects/mathGame/"},
+    { name: "מזג אוויר", url: "https://star69995.github.io/star-site/js-projects/weather/"},
+    { name: "מחשבון קרשים", url: "https://star69995.github.io/star-site/js-projects/wood-calc/"},
 ];
 
 const container = document.getElementById('projects-container');
